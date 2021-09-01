17d9b04a6c33244d993784b66864a23a9cdba535
EricRene <58518322+EricRene@users.noreply.github.com>
2021-08-31 19:54:15 -0500

    Updated Iam Role Resource section

--- main.tf
+++ main.tf
@@ -16,84 +16,6 @@ provider "aws" {
 
 #Data sources
 
-data "aws_iam_policy_document" "eks_cluster_role_policy" {
-  statement{
-            Effect = "Allow"
-            Action = [
-                "autoscaling:DescribeAutoScalingGroups"
-                ,"autoscaling:UpdateAutoScalingGroup"
-                ,"ec2:AttachVolume"
-                ,"ec2:AuthorizeSecurityGroupIngress"
-                ,"ec2:CreateRoute"
-                ,"ec2:CreateSecurityGroup"
-                ,"ec2:CreateTags"
-                ,"ec2:CreateVolume"
-                ,"ec2:DeleteRoute"
-                ,"ec2:DeleteSecurityGroup"
-                ,"ec2:DeleteVolume"
-                ,"ec2:DescribeInstances"
-                ,"ec2:DescribeRouteTables"
-                ,"ec2:DescribeSecurityGroups"
-                ,"ec2:DescribeSubnets"
-                ,"ec2:DescribeVolumes"
-                ,"ec2:DescribeVolumesModifications"
-                ,"ec2:DescribeVpcs"
-                ,"ec2:DescribeDhcpOptions"
-                ,"ec2:DescribeNetworkInterfaces"
-                ,"ec2:DetachVolume"
-                ,"ec2:ModifyInstanceAttribute"
-                ,"ec2:ModifyVolume"
-                ,"ec2:RevokeSecurityGroupIngress"
-                ,"ec2:DescribeAccountAttributes"
-                ,"ec2:DescribeAddresses"
-                ,"ec2:DescribeInternetGateways"
-                ,"elasticloadbalancing:AddTags"
-                ,"elasticloadbalancing:ApplySecurityGroupsToLoadBalancer"
-                ,"elasticloadbalancing:AttachLoadBalancerToSubnets"
-                ,"elasticloadbalancing:ConfigureHealthCheck"
-                ,"elasticloadbalancing:CreateListener"
-                ,"elasticloadbalancing:CreateLoadBalancer"
-                ,"elasticloadbalancing:CreateLoadBalancerListeners"
-                ,"elasticloadbalancing:CreateLoadBalancerPolicy"
-                ,"elasticloadbalancing:CreateTargetGroup"
-                ,"elasticloadbalancing:DeleteListener"
-                ,"elasticloadbalancing:DeleteLoadBalancer"
-                ,"elasticloadbalancing:DeleteLoadBalancerListeners"
-                ,"elasticloadbalancing:DeleteTargetGroup"
-                ,"elasticloadbalancing:DeregisterInstancesFromLoadBalancer"
-                ,"elasticloadbalancing:DeregisterTargets"
-                ,"elasticloadbalancing:DescribeListeners"
-                ,"elasticloadbalancing:DescribeLoadBalancerAttributes"
-                ,"elasticloadbalancing:DescribeLoadBalancerPolicies"
-                ,"elasticloadbalancing:DescribeLoadBalancers"
-                ,"elasticloadbalancing:DescribeTargetGroupAttributes"
-                ,"elasticloadbalancing:DescribeTargetGroups"
-                ,"elasticloadbalancing:DescribeTargetHealth"
-                ,"elasticloadbalancing:DetachLoadBalancerFromSubnets"
-                ,"elasticloadbalancing:ModifyListener"
-                ,"elasticloadbalancing:ModifyLoadBalancerAttributes"
-                ,"elasticloadbalancing:ModifyTargetGroup"
-                ,"elasticloadbalancing:ModifyTargetGroupAttributes"
-                ,"elasticloadbalancing:RegisterInstancesWithLoadBalancer"
-                ,"elasticloadbalancing:RegisterTargets"
-                ,"elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer"
-                ,"elasticloadbalancing:SetLoadBalancerPoliciesOfListener"
-                ,"kms:DescribeKey"
-            ]
-             Resource = "*"
-        }
-        {
-             Effect = "Allow"
-             Action = "iam:CreateServiceLinkedRole"
-             Resource = "*"
-             Condition = {
-                 StringEquals = {
-                     iam:AWSServiceName = "elasticloadbalancing.amazonaws.com"
-                }
-            }
-  }
-}
-
 
 
 #VPC resource
@@ -120,8 +42,94 @@ resource "aws_subnet" "main" {
 #IAM Role resource 
 
 resource "aws_iam_role" "eks_cluster_role" {
-  name               = "eks_cluster_role"
-  assume_role_policy = data.aws_iam_policy_document.eks_cluster_role_policy.json
+  name = "eks_cluster_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "autoscaling:DescribeAutoScalingGroups",
+                "autoscaling:UpdateAutoScalingGroup",
+                "ec2:AttachVolume",
+                "ec2:AuthorizeSecurityGroupIngress",
+                "ec2:CreateRoute",
+                "ec2:CreateSecurityGroup",
+                "ec2:CreateTags",
+                "ec2:CreateVolume",
+                "ec2:DeleteRoute",
+                "ec2:DeleteSecurityGroup",
+                "ec2:DeleteVolume",
+                "ec2:DescribeInstances",
+                "ec2:DescribeRouteTables",
+                "ec2:DescribeSecurityGroups",
+                "ec2:DescribeSubnets",
+                "ec2:DescribeVolumes",
+                "ec2:DescribeVolumesModifications",
+                "ec2:DescribeVpcs",
+                "ec2:DescribeDhcpOptions",
+                "ec2:DescribeNetworkInterfaces",
+                "ec2:DetachVolume",
+                "ec2:ModifyInstanceAttribute",
+                "ec2:ModifyVolume",
+                "ec2:RevokeSecurityGroupIngress",
+                "ec2:DescribeAccountAttributes",
+                "ec2:DescribeAddresses",
+                "ec2:DescribeInternetGateways",
+                "elasticloadbalancing:AddTags",
+                "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
+                "elasticloadbalancing:AttachLoadBalancerToSubnets",
+                "elasticloadbalancing:ConfigureHealthCheck",
+                "elasticloadbalancing:CreateListener",
+                "elasticloadbalancing:CreateLoadBalancer",
+                "elasticloadbalancing:CreateLoadBalancerListeners",
+                "elasticloadbalancing:CreateLoadBalancerPolicy",
+                "elasticloadbalancing:CreateTargetGroup",
+                "elasticloadbalancing:DeleteListener",
+                "elasticloadbalancing:DeleteLoadBalancer",
+                "elasticloadbalancing:DeleteLoadBalancerListeners",
+                "elasticloadbalancing:DeleteTargetGroup",
+                "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+                "elasticloadbalancing:DeregisterTargets",
+                "elasticloadbalancing:DescribeListeners",
+                "elasticloadbalancing:DescribeLoadBalancerAttributes",
+                "elasticloadbalancing:DescribeLoadBalancerPolicies",
+                "elasticloadbalancing:DescribeLoadBalancers",
+                "elasticloadbalancing:DescribeTargetGroupAttributes",
+                "elasticloadbalancing:DescribeTargetGroups",
+                "elasticloadbalancing:DescribeTargetHealth",
+                "elasticloadbalancing:DetachLoadBalancerFromSubnets",
+                "elasticloadbalancing:ModifyListener",
+                "elasticloadbalancing:ModifyLoadBalancerAttributes",
+                "elasticloadbalancing:ModifyTargetGroup",
+                "elasticloadbalancing:ModifyTargetGroupAttributes",
+                "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+                "elasticloadbalancing:RegisterTargets",
+                "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
+                "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
+                "kms:DescribeKey"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "iam:CreateServiceLinkedRole",
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "iam:AWSServiceName": "elasticloadbalancing.amazonaws.com"
+                }
+            }
+        }
+    ]
+})
+
+  tags = {
+    Name = "eks_cluster_role"
+  }
 }
 
 #Security Groups Resource
