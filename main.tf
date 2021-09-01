c95d81427981481010a407c7f812a7d411446c8c
Ubuntu <ubuntu@ip-172-31-25-236.us-east-2.compute.internal>
2021-09-01 00:46:53 +0000

    updated main.tf

--- main.tf
+++ main.tf
@@ -8,15 +8,91 @@ variable "cluster_name" {
 
 
 provider "aws" {
-  access_key = ""
-  secret_key = "" 
+  access_key = "AKIARU7V64ZGRSNAAT45"
+  secret_key = "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" 
   region= var.region
 }
 
 
 #Data sources
 
-
+data "aws_iam_policy_document" "eks_cluster_role_policy" {
+  statement{
+            Effect = "Allow"
+            Action = [
+                "autoscaling:DescribeAutoScalingGroups"
+                ,"autoscaling:UpdateAutoScalingGroup"
+                ,"ec2:AttachVolume"
+                ,"ec2:AuthorizeSecurityGroupIngress"
+                ,"ec2:CreateRoute"
+                ,"ec2:CreateSecurityGroup"
+                ,"ec2:CreateTags"
+                ,"ec2:CreateVolume"
+                ,"ec2:DeleteRoute"
+                ,"ec2:DeleteSecurityGroup"
+                ,"ec2:DeleteVolume"
+                ,"ec2:DescribeInstances"
+                ,"ec2:DescribeRouteTables"
+                ,"ec2:DescribeSecurityGroups"
+                ,"ec2:DescribeSubnets"
+                ,"ec2:DescribeVolumes"
+                ,"ec2:DescribeVolumesModifications"
+                ,"ec2:DescribeVpcs"
+                ,"ec2:DescribeDhcpOptions"
+                ,"ec2:DescribeNetworkInterfaces"
+                ,"ec2:DetachVolume"
+                ,"ec2:ModifyInstanceAttribute"
+                ,"ec2:ModifyVolume"
+                ,"ec2:RevokeSecurityGroupIngress"
+                ,"ec2:DescribeAccountAttributes"
+                ,"ec2:DescribeAddresses"
+                ,"ec2:DescribeInternetGateways"
+                ,"elasticloadbalancing:AddTags"
+                ,"elasticloadbalancing:ApplySecurityGroupsToLoadBalancer"
+                ,"elasticloadbalancing:AttachLoadBalancerToSubnets"
+                ,"elasticloadbalancing:ConfigureHealthCheck"
+                ,"elasticloadbalancing:CreateListener"
+                ,"elasticloadbalancing:CreateLoadBalancer"
+                ,"elasticloadbalancing:CreateLoadBalancerListeners"
+                ,"elasticloadbalancing:CreateLoadBalancerPolicy"
+                ,"elasticloadbalancing:CreateTargetGroup"
+                ,"elasticloadbalancing:DeleteListener"
+                ,"elasticloadbalancing:DeleteLoadBalancer"
+                ,"elasticloadbalancing:DeleteLoadBalancerListeners"
+                ,"elasticloadbalancing:DeleteTargetGroup"
+                ,"elasticloadbalancing:DeregisterInstancesFromLoadBalancer"
+                ,"elasticloadbalancing:DeregisterTargets"
+                ,"elasticloadbalancing:DescribeListeners"
+                ,"elasticloadbalancing:DescribeLoadBalancerAttributes"
+                ,"elasticloadbalancing:DescribeLoadBalancerPolicies"
+                ,"elasticloadbalancing:DescribeLoadBalancers"
+                ,"elasticloadbalancing:DescribeTargetGroupAttributes"
+                ,"elasticloadbalancing:DescribeTargetGroups"
+                ,"elasticloadbalancing:DescribeTargetHealth"
+                ,"elasticloadbalancing:DetachLoadBalancerFromSubnets"
+                ,"elasticloadbalancing:ModifyListener"
+                ,"elasticloadbalancing:ModifyLoadBalancerAttributes"
+                ,"elasticloadbalancing:ModifyTargetGroup"
+                ,"elasticloadbalancing:ModifyTargetGroupAttributes"
+                ,"elasticloadbalancing:RegisterInstancesWithLoadBalancer"
+                ,"elasticloadbalancing:RegisterTargets"
+                ,"elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer"
+                ,"elasticloadbalancing:SetLoadBalancerPoliciesOfListener"
+                ,"kms:DescribeKey"
+            ]
+             Resource = "*"
+        }
+        {
+             Effect = "Allow"
+             Action = "iam:CreateServiceLinkedRole"
+             Resource = "*"
+             Condition = {
+                 StringEquals = {
+                     iam:AWSServiceName = "elasticloadbalancing.amazonaws.com"
+                }
+            }
+  }
+}
 
 
 
@@ -24,12 +100,16 @@ provider "aws" {
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "eks_vpc"
+  }
 }
 
 #Subnet resource
 
 resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
@@ -37,148 +117,41 @@ resource "aws_subnet" "main" {
   }
 }
 
-
-#IAM Role
+#IAM Role resource 
 
 resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks_cluster_role"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2:CreateNetworkInterface",
-                "ec2:DeleteNetworkInterface",
-                "ec2:DetachNetworkInterface",
-                "ec2:ModifyNetworkInterfaceAttribute",
-                "ec2:DescribeInstances",
-                "ec2:DescribeNetworkInterfaces",
-                "ec2:DescribeSecurityGroups",
-                "ec2:DescribeSubnets",
-                "ec2:DescribeVpcs",
-                "ec2:CreateNetworkInterfacePermission",
-                "iam:ListAttachedRolePolicies",
-                "ec2:CreateSecurityGroup"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2:DeleteSecurityGroup",
-                "ec2:RevokeSecurityGroupIngress",
-                "ec2:AuthorizeSecurityGroupIngress"
-            ],
-            "Resource": "arn:aws:ec2:*:*:security-group/*",
-            "Condition": {
-                "ForAnyValue:StringLike": {
-                    "ec2:ResourceTag/Name": "eks-cluster-sg*"
-                }
-            }
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2:CreateTags",
-                "ec2:DeleteTags"
-            ],
-            "Resource": [
-                "arn:aws:ec2:*:*:vpc/*",
-                "arn:aws:ec2:*:*:subnet/*"
-            ],
-            "Condition": {
-                "ForAnyValue:StringLike": {
-                    "aws:TagKeys": [
-                        "kubernetes.io/cluster/*"
-                    ]
-                }
-            }
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2:CreateTags",
-                "ec2:DeleteTags"
-            ],
-            "Resource": [
-                "arn:aws:ec2:*:*:security-group/*"
-            ],
-            "Condition": {
-                "ForAnyValue:StringLike": {
-                    "aws:TagKeys": [
-                        "kubernetes.io/cluster/*"
-                    ],
-                    "aws:RequestTag/Name": "eks-cluster-sg*"
-                }
-            }
-        },
-        {
-            "Effect": "Allow",
-            "Action": "route53:AssociateVPCWithHostedZone",
-            "Resource": "arn:aws:route53:::hostedzone/*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "logs:CreateLogGroup",
-            "Resource": "arn:aws:logs:*:*:log-group:/aws/eks/*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogStream",
-                "logs:DescribeLogStreams"
-            ],
-            "Resource": "arn:aws:logs:*:*:log-group:/aws/eks/*:*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "logs:PutLogEvents",
-            "Resource": "arn:aws:logs:*:*:log-group:/aws/eks/*:*:*"
-        }
-    ]
-})
-
-  tags = {
-    tag-key = "tag-value"
-  }
+  name               = "eks_cluster_role"
+  assume_role_policy = data.aws_iam_policy_document.eks_cluster_role_policy.json
 }
-
-
-#Subnets Resource
-
 
 #Security Groups Resource
 
 
 #EKS cluster  Resource
 
-resource "aws_eks_cluster" "my_test_cluster" {
-name     = var.cluster_name
-  role_arn = aws_iam_role.my_test_cluster.arn
-
-  vpc_config {
-    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
-  }
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
-  ]
-}
-
-output "endpoint" {
-  value = aws_eks_cluster.my_test_cluster.endpoint
-}
-
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.my_test_cluster.certificate_authority[0].data
-}
+#resource "aws_eks_cluster" "my_test_cluster" {
+#name     = var.cluster_name
+#  role_arn = aws_iam_role.my_test_cluster.arn
+#
+#  vpc_config {
+#    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
+#  }
+#
+#  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+#  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+#  depends_on = [
+#    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+#    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+#  ]
+#}
+#
+#output "endpoint" {
+#  value = aws_eks_cluster.my_test_cluster.endpoint
+#}
+#
+#output "kubeconfig-certificate-authority-data" {
+#  value = aws_eks_cluster.my_test_cluster.certificate_authority[0].data
+#}
   
-}
+#}
 
