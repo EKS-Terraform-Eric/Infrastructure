e299558235ba09a037e783e156bad78fda140787
EricRene <ericrene.henriquezpalomar@gmail.com>
2021-09-08 19:00:52 -0500

    Fixed tag in vpc

--- main.tf
+++ main.tf
@@ -25,11 +25,12 @@ provider "aws" {
 resource "aws_vpc" "eks_vpc" {
   cidr_block = "10.0.0.0/16"
 
-  tags =
+  tags = {
      map(
        Name = terraform-eks-vpc
       kubernetes.io/cluster/var.cluster_name = "shared"
      )
+  }
 }
 resource "aws_subnet" "eks_subnet" {
   count = 2
